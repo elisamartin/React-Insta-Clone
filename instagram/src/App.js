d13cc710a9e1e0ace1d78bfece6770c276369a93
elisamartin <elisamv93@gmail.com>
2019-02-20 14:33:55 +0100

    Day III: Created PostsPage and re-factored App component

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,47 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import SearchBar from './components/SearchBar/SearchBar';
-import PostContainer from './components/PostContainer/PostContainer';
+import PostsPage from './components/PostContainer/PostsPage';
 import dummyData from './dummy-data';
 
-class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			posts: [],
-			search: ''
-		};
-	}
-
-	componentDidMount() {
-		this.setState({
-			posts: dummyData
-		});
-	}
-
-	handleSearch = (event) => {
-		this.setState({
-			posts: this.state.posts.filter((item) => {
-				return item.username.includes(event.target.value);
-			})
-		});
-	};
-
-	render() {
-		return (
-			<div className="app-container">
-				<SearchBar
-					searchText={this.state.search}
-					handleSearch={this.handleSearch}
-					searchItem={this.searchItem}
-				/>
-				{this.state.posts.map((post, timestamp) => {
-					return <PostContainer key={timestamp} post={post} />;
-				})}
-			</div>
-		);
-	}
-}
+const App = () => {
+	return <PostsPage />;
+};
 
 export default App;
---
+++ instagram/src/components/PostContainer/PostsPage.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import '../../App.css';
+
+import SearchBar from '../SearchBar/SearchBar';
+import PostContainer from './PostContainer';
+import dummyData from '../../dummy-data';
+//import PropTypes from 'prop-types';
+
+class PostsPage extends Component {
+	constructor() {
+		super();
+		this.state = {
+			posts: [],
+			search: ''
+		};
+	}
+
+	componentDidMount() {
+		this.setState({
+			posts: dummyData
+		});
+	}
+
+	handleSearch = (event) => {
+		this.setState({
+			posts: this.state.posts.filter((item) => {
+				return item.username.includes(event.target.value);
+			})
+		});
+	};
+
+	render() {
+		return (
+			<div className="app-container">
+				<SearchBar
+					searchText={this.state.search}
+					handleSearch={this.handleSearch}
+					searchItem={this.searchItem}
+				/>
+				{this.state.posts.map((post, timestamp) => {
+					return <PostContainer key={timestamp} post={post} />;
+				})}
+			</div>
+		);
+	}
+}
+
+export default PostsPage;
