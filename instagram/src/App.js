649514e83e81837ecd005428875d3bc8770cc056
elisamartin <elisamv93@gmail.com>
2019-02-20 14:54:17 +0100

    Day III: Created Authenticate.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-
 import PostsPage from './components/PostContainer/PostsPage';
-import dummyData from './dummy-data';
+// import dummyData from './dummy-data';
+import Authenticate from './components/Authentication/Authenticate';
 
 const App = () => {
 	return <PostsPage />;
 };
 
-export default App;
+export default Authenticate(App);
